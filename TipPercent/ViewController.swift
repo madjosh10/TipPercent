f86dd41dda89c8a15fa604f66c2d380bd313b9a2
Joshua Madrigal <joshua@joshs.local>
2020-01-21 13:37:30 -0600

    New ups

---
+++ TipPercent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipPercent
+//
+//  Created by Joshua Madrigal on 1/16/20.
+//  Copyright © 2020 Joshua Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
